681b9c27b063ba03296fc4d83e0a46da5ea49169
Gabriel Evaristo <gabriel.evaristo.2004@gmail.com>
2024-11-22 21:12:16 -0300

    initial commit

---
+++ src/main/java/io/github/gabrielhenriquehe/itajufacil/ItajufacilApplication.java
@@ -0,0 +1,13 @@
+package io.github.gabrielhenriquehe.itajufacil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItajufacilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItajufacilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/gabrielhenriquehe/itajufacil/ItajufacilApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.gabrielhenriquehe.itajufacil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItajufacilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
