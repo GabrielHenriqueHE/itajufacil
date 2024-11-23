e342ca70080efaad1a9fdf1f29c781500bdd377d
Gabriel Evaristo <gabriel.evaristo.2004@gmail.com>
2024-11-22 21:58:54 -0300

    feat: implementando entidade Usuario

---
+++ src/main/java/io/github/gabrielhenriquehe/itajufacil/domain/produto/Usuario.java
@@ -0,0 +1,43 @@
+package io.github.gabrielhenriquehe.itajufacil.domain.produto;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Entity
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Usuario implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false, unique = true)
+    private String telefone;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String senha;
+
+    public Usuario(String nome, String telefone, String email, String senha) {
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+        this.senha = senha;
+    }
+}
